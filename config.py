88a24889749e5d3d3c38165d5f79dd6f58c837f0
Alina Ermilova <alina.ermilova92@gmail.com>
2026-09-22 23:43:11 +0300

    con cha

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-LOGIN = os.getenv('login')
-PASSWORD = os.getenv('password')
-BASE_URL = os.getenv('base_url', 'http://prestashop:80')
+LOGIN = os.getenv('LOGIN')
+PASSWORD = os.getenv('PASSWORD')
+BASE_URL = os.getenv('BASE_URL', 'http://prestashop:80')
 
 if not all([LOGIN, PASSWORD, BASE_URL]):
     raise ValueError('Не все переменные окружения загружены! Проверьте .env файл.')
